99dfa45de542349af2eb8b9351e11f22ea39a722
hippieZhou <hippiezhou@outlook.com>
2019-10-14 22:18:36 +0800

    update

---
+++ src/011.container-with-most-water.py
@@ -0,0 +1,12 @@
+# https://leetcode-cn.com/problems/container-with-most-water/
+
+
+class Solution:
+    # 超时
+    def maxArea(self, height: list) -> int:
+        _max = 0
+        for i in range(len(height) - 1):
+            for j in range(1, len(height)):
+                t = (j-i) * min(height[i], height[j])
+                max = max(_max, t)
+        return _max
