2ebe4077695931904356bc6fc9f021fb5e40f8ed
hippieZhou <hippiezhou@outlook.com>
2019-10-14 11:37:53 +0800

    update

--- src/189.rotate-array.py
+++ src/189.rotate-array.py
@@ -1,9 +1,17 @@
-# https://leetcode.com/problems/rotate-array/ 
+# https://leetcode.com/problems/rotate-array/
 
 
 class Solution:
+    # def rotate(self, nums: list, k: int) -> None:
+    #     while k:
+    #         nums.insert(0, nums.pop())
+    #         k -= 1
+
+    # def rotate(self, nums: list, k: int) -> None:
+    #     k %= len(nums)
+    #     nums[:] = nums[::-1]
+    #     nums[:] = nums[:k][::-1] + nums[k:][-1]
+
     def rotate(self, nums: list, k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        nums = nums[k+1:].append(nums[0:k])
+        k %= len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
