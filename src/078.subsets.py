aefde3bd6bac02c52ad766459697928a0d0ac3b7
hippieZhou <hippiezhou@outlook.com>
2019-10-24 19:40:39 +0800

    update

--- src/078.subsets.py
+++ src/078.subsets.py
@@ -1,3 +1,10 @@
 class Solution:
     def subsets(self, nums: list) -> list:
-        pass
+        subs = [[]]
+        for num in nums:
+            subs += [[num] + i for i in subs]
+        return subs
+
+
+v = Solution().subsets([1, 2, 3])
+print(v)
