b3d9f9853e59197b12b2b420d65c6d2ee0ea4f56
hippieZhou <hippiezhou@outlook.com>
2019-10-31 22:49:05 +0800

    update

--- src/122.best-time-to-buy-and-sell-stock-ii.py
+++ src/122.best-time-to-buy-and-sell-stock-ii.py
@@ -5,3 +5,6 @@ class Solution:
             if prices[i] > prices[i-1]:
                 profit = profit + prices[i] - prices[i-1]
         return profit
+
+    def maxProfit(self, prices: list) -> int:
+        return sum(max(prices[i] - prices[i-1], 0) for i in range(1, len(prices)))
