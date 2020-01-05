7c02cbb6edbf996b69791746dfaa22165b70391a
hippieZhou <hippiezhou@outlook.com>
2020-01-05 16:30:02 +0800

    update

--- src/062.unique-paths.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        ans = [[1 for x in range(n)] for x in range(m)]
-        for i in range(1, m):
-            for j in range(1, n):
-                ans[i][j] = ans[i][j-1] + ans[i-1][j]
-        return ans[-1][-1]
--- src/169.majority-element.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        return list(sorted(nums))[len(nums)//2]
